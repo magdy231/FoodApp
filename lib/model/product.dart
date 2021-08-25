5dd95bd615132dc90c04067d56a37dcd6e9a39fb
Ahmed Magdy <ahmedmagdy2319@gmail.com>
2021-08-25 02:20:40 +0200

    second

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -43,5 +43,14 @@ List<Product> myProduct = [
       mediumPrice: 40,
       bigPrice: 60,
       ID: 1),
-
+  Product(
+      Name: 'Passta meet',
+      subTitle: 'godzela',
+      describtion: 'ma3krona with souce meet el3zeeem ',
+      imageUrl:
+      'https://static.remove.bg/remove-bg-web/207b10c4ce48e7dca1441ee119b7f52754f487fd/assets/start-0e837dcc57769db2306d8d659f53555feb500b3c5d456879b9c843d1872e7baa.jpg',
+      smallPrice: 20,
+      mediumPrice: 40,
+      bigPrice: 60,
+      ID: 1),
 ];
